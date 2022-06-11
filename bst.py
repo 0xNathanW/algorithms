055e2e2075d3027164e2902418a7775bb4cf5899
NathanW <86011312+0xNathanW@users.noreply.github.com>
2022-06-11 23:28:49 +0100

    notes

--- bst.py
+++ bst.py
@@ -1,8 +1,9 @@
 from collections import deque
+from re import X
 
 class Node:
 
-    def __init__(self, key: int):
+    def __init__(self, key=None):
 
         self.key = key
         self.p = None
@@ -20,9 +21,9 @@ class Tree:
             x = self.root
 
         if x != None:
-            self.inorder(x.left)
+            self.inorder(x.l)
             print(x.key)
-            self.inorder(x.right)
+            self.inorder(x.r)
 
     def search_recurse(self, k, x="root"):
         if x=="root":
@@ -30,22 +31,98 @@ class Tree:
         if x == None or k == x.key:
             return x
         
-        return self.search(k, x.left) if k<x.key else self.search(k, x.right)
+        return self.search(k, x.l) if k<x.key else self.search(k, x.r)
 
     def search(self, k):
         x = self.root
         while x is not None and k != x.key:
-            x = x.left if k<x.key else x.right
+            x = x.l if k<x.key else x.r
         return x
 
     def insert(self, new):
         z = Node(new)
         x, y = self.root, None
 
-        while x != None:
+        while x is not None:
             y=x
-            if z.key < x.key:
-                x = x.left
+            x = x.l if z.key < x.key else x.r
+        
+        z.p = y
+        if y is None:
+            self.root = z
+        elif z.key < y.key:
+            y.l = z
+        else:
+            y.r = z
+
+    def insert_list(self, new):
+        for n in new:
+            self.insert(n)
+
+    def minimum(self, x="root"):
+        
+        if x == "root":
+            x = self.root
+
+        while x.l is not None:
+            x = x.l
+        return x
+
+    def maximum(self, x="root"):
+        
+        if x == "root":
+            x = self.root
+
+        while x.r is not None:
+            x = x.r
+        return x
+
+    def successor(self, x):
+        if x.r is not None:
+            return self.minimum(x.r)
+        
+        y = x.p
+        while y is not None and x == y.r:
+            x = y
+            y = y.p
+        return y
+
+    def preccessor(self, x):
+        if x.l is not None:
+            return self.maximum(x.l)
+
+        y = x.p
+        while y is not None and x == y.l:
+            x = y
+            y = y.p
+        return y
+
+    def transplant(self, u, v):
+        # Replaces subtree at node u with that rooted at node v.
+        if u.p is None:
+            self.root = v
+        elif u == u.p.l:
+            u.p.l = v
+        else: 
+            u.p.r = v
+        if v is not None:
+            v.p = u.p
+
+    def delete(self, z):
+        
+        if z.l is None:
+            self.transplant(z, z.r)
+        elif z.r is None:
+            self.transplant(z, z.l)
+        else:
+            y = self.minimum(z.r)
+            if y != z.r:
+                self.transplant(y, y.r)
+                y.r = z.r
+                y.r.p = y
+            self.transplant(z, y)
+            y.l = z.l
+            y.l.p = y
 
     def print_tree(self):
         root = self.root
@@ -72,5 +149,9 @@ class Tree:
             print("|")
             base //= 2
 
-test = Tree(Node(6))
-
+tree = Tree(Node(6))
+tree.insert_list([5, 7, 2, 5, 8, 1, 3, 4])
+tree.print_tree()
+tree.delete(tree.search(5))
+print("="*20)
+tree.print_tree()
---
+++ notes/keywords.md
@@ -0,0 +1,68 @@
+# Indentification Words #
+
+## Top-K ##
+* Heap.
+
+## How many ways...##
+* DFS
+* Dynaminc programming
+
+## Substring ##
+* Sliding windows
+
+## Palindrome ##
+* Two pointers
+* DFS
+* Dynamic programming
+
+## Tree ##
+* "Shortest.." or "Level order" -> BFS
+* Otherwise DFS
+
+## Parentheses ##
+* Stack
+
+## Sub-array ##
+* Prefix sum
+* Hashmap
+
+## X sum ##
+* two pointer, fix variable if x>2
+
+## Max/longest sequence ##
+* Dynamic programming/DFS
+* Mono deque
+
+## Minimum/shortest ##
+* Dynamic programming/DFS
+* BFS
+
+## Partition/split..array/string ##
+* DFS
+
+## Subsequence ##
+* Dynamic programming/DFS
+* Sliding window
+
+## Matrix ##
+* DFS/BFS
+* Dynamic programming
+
+## Jump ##
+* Greedy/DP
+
+## Game ##
+* DP
+
+## "Connected component", "Cut/remove" "Regions/groups/connections" ##
+* Union find
+
+## Transitive relationship ##
+If the items are related to one another and the relationship is transitive, then chances are we can build a graph and use BFS or Union Find.
+
+* String converting to another, BFS: Word Ladder
+* String converting to another, BFS, Union Find: Sentence Similarity
+* Numbers having divisional relationship, BFS, Union Find: Evaluate Division
+
+## Interval ##
+* Greedy
